fb3795be2a6da223b5e2431918d68fe4f1e87aa1
Andre Alvim Ribeiro <andre2ar@outlook.com>
2021-07-12 16:29:00 -0300

    RentalX

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import CategoriesRepository from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
+export default class ImportCategoryUseCase {
+    constructor(
+        private categoriesRepository: CategoriesRepository
+    ) {}
+
+    parseCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+            const parseFile = csvParse();
+
+            stream.pipe(parseFile);
+
+            parseFile.on('data', async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description
+                })
+            }).on('end', () => {
+                resolve(categories);
+            }).on('error', (err) => {
+                reject(err)
+            });
+        });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.parseCategories(file);
+
+        categories.forEach(category => {
+            const {name, description} = category;
+            const categoryExists = this.categoriesRepository.findByName(name);
+            if(!categoryExists) {
+                this.categoriesRepository.create({
+                    name,
+                    description
+                });
+            }
+        })
+    }
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import {Router} from "express";
 import {createCategoryController} from "../modules/cars/useCases/createCategory";
 import {listCategoriesController} from "../modules/cars/useCases/listCategories";
+import multer from "multer";
+import {importCategoryController} from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.get('/', (request, response) => {
     return listCategoriesController.handle(request, response);
@@ -10,6 +15,10 @@ categoriesRoutes.get('/', (request, response) => {
 
 categoriesRoutes.post('/', (request, response) => {
     return createCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+    return importCategoryController.handle(request, response);
 });
 
 export default categoriesRoutes;
