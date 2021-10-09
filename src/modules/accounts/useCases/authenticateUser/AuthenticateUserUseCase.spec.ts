6a6b4d4f3ea279e50d879b8572ee418537b4ac2a
Andre Alvim Ribeiro <andre2ar@outlook.com>
2021-10-09 19:54:20 -0300

    Authenticate Test

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -34,4 +34,31 @@ describe("Authenticate User", () => {
 
         expect(response).toHaveProperty("token");
     });
+
+    it('should not be able to authenticate a nonexistent user', async () => {
+        await expect(async () => {
+            await authenticateUserUseCase.execute({
+                email: "false@email.com",
+                password: "123456"
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to authenticate with incorrect password', async () => {
+        await expect(async () => {
+            const user: ICreateUserDTO = {
+                email: "user@test.com",
+                name: "User",
+                password: "123456",
+                driver_license: "123456"
+            };
+
+            await createUserUseCase.execute(user);
+
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: "wrong"
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
 });
