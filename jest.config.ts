429ea3a0a7aae22dde140fd096e2a9fc2062cf38
Andre Alvim Ribeiro <andre2ar@outlook.com>
2021-10-10 10:22:21 -0300

    Import refactoring

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,6 @@
+import {pathsToModuleNameMapper} from "ts-jest/utils";
+import {compilerOptions} from "./tsconfig.json";
+
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
@@ -81,14 +84,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    "@modules/(.*)": ["<rootDir>/src/modules/$1"],
-    "@config/(.*)": ["<rootDir>/src/config/$1"],
-    "@shared/(.*)": ["<rootDir>/src/shared/$1"],
-    "@errors/(.*)": ["<rootDir>/src/errors/$1"],
-    "@middlewares/(.*)": ["<rootDir>/src/modules/$1"],
-    "@utils/(.*)": ["<rootDir>/src/utils/$1"],
-  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/"
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
